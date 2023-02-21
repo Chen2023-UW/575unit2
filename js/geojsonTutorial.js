875604a4635104c9dfe470f54ab3b17c6788e531
Chen2023-UW <jchen2323@wisc.edu>
2023-02-21 13:34:55 -0600

    edit1

---
+++ js/geojsonTutorial.js
@@ -0,0 +1,5 @@
+var map = L.map('map').setView([51.505, -0.09], 13);
+var titleLayer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+}).addTo(map);
+
