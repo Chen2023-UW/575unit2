c14fda04a95adeb6383701ea0b17095e3fed4963
Chen2023-UW <jchen2323@wisc.edu>
2023-03-05 21:11:29 -0600

    Activity6

--- js/example3.19.js
+++ js/example3.19.js
@@ -1,4 +1,4 @@
-//declare map variable globally so all functions have access, declare minvalue for leater use
+//declare map variable globally so all functions have access
 var map;
 var minValue;
 
@@ -6,38 +6,34 @@ function createMap(){
 
     //create the map
     map = L.map('map', {
-        center: [-3,-60],
-        zoom: 3
+        center: [0, 0],
+        zoom: 1
     });
 
     //add OSM base tilelayer
-    L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain-background/{z}/{x}/{y}{r}.{ext}', {
-        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        subdomains: 'abcd',
-        minZoom: 0,
-        maxZoom: 18,
-        ext: 'png'
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
     }).addTo(map);
 
     //call getData function
     getData(map);
 };
 
-function calculateMinValue(data){
+function calcMinValue(data){
     //create empty array to store all data values
-    var allvalues = [];
-    //loop through each country
-    for(var Country of data.features){
-        for(var year = 1999; year <= 2018; year+=1){
-            //get immigrant for current year
-            var value = Country.properties["imm_" + year];
-            //add value to array
-            if (value > 0)
-              allvalues.push(value);
+    var allValues = [];
+    //loop through each city
+    for(var city of data.features){
+        //loop through each year
+        for(var year = 1985; year <= 2015; year+=5){
+              //get population for current year
+              var value = city.properties["Pop_"+ year];
+              //add value to array
+              allValues.push(value);
         }
     }
     //get minimum value of our array
-    var minValue = Math.min(...allvalues)
+    var minValue = Math.min(...allValues)
 
     return minValue;
 }
@@ -45,18 +41,15 @@ function calculateMinValue(data){
 //calculate the radius of each proportional symbol
 function calcPropRadius(attValue) {
     //constant factor adjusts symbol sizes evenly
-    var minRadius = 4;
+    var minRadius = 5;
     //Flannery Apperance Compensation formula
-    if (attValue > 0)
-      var radius = 1.0 * Math.pow(attValue/minValue,0.2) * minRadius
-    else
-      var radius = 3
+    var radius = 1.0083 * Math.pow(attValue/minValue,0.5715) * minRadius
 
     return radius;
 };
 
 //function to convert markers to circle markers and add popups
-function pointToLayer(feature, latlng, attributes){
+function pointToLayer(feature, latlng,attributes){
     //Determine which attribute to visualize with proportional symbols
     var attribute = attributes[0];
     //check
@@ -67,8 +60,8 @@ function pointToLayer(feature, latlng, attributes){
         fillColor: "#ff7800",
         color: "#000",
         weight: 1,
-        opacity: 0.8,
-        fillOpacity: 0.6
+        opacity: 1,
+        fillOpacity: 0.8
     };
 
     //For each feature, determine its value for the selected attribute
@@ -80,12 +73,12 @@ function pointToLayer(feature, latlng, attributes){
     //create circle marker layer
     var layer = L.circleMarker(latlng, options);
 
-    //build popup content string starting with country...Example 2.1 line 24
-    var popupContent = "<p><b>Country:</b> " + feature.properties.Country + "</p>";
+    //build popup content string starting with city...Example 2.1 line 24
+    var popupContent = "<p><b>City:</b> " + feature.properties.City + "</p>";
 
     //add formatted attribute to popup content string
-    /*var year = attribute.split("_")[1];
-    popupContent += "<p><b>Population in " + year + ":</b> " + feature.properties[attribute] + " million</p>";*/
+    var year = attribute.split("_")[1];
+    popupContent += "<p><b>Population in " + year + ":</b> " + feature.properties[attribute] + " million</p>";
 
     //bind the popup to the circle marker
     layer.bindPopup(popupContent, {
@@ -117,12 +110,12 @@ function updatePropSymbols(attribute){
            var radius = calcPropRadius(props[attribute]);
            layer.setRadius(radius);
 
-           //add country to popup content string
-           var popupContent = "<p><b>Country:</b> " + props.country + "</p>";
+           //add city to popup content string
+           var popupContent = "<p><b>City:</b> " + props.City + "</p>";
 
            //add formatted attribute to panel content string
-           /*var year = attribute.split("_")[1];
-           popupContent += "<p><b>Population in " + year + ":</b> " + props[attribute] + " million</p>";*/
+           var year = attribute.split("_")[1];
+           popupContent += "<p><b>Population in " + year + ":</b> " + props[attribute] + " million</p>";
 
            //update popup with new content
            popup = layer.getPopup();
@@ -206,13 +199,13 @@ function createSequenceControls(attributes){
 
 function getData(map){
     //load the data
-    fetch("data/immc.geojson")
+    fetch("data/MegaCities.geojson")
         .then(function(response){
             return response.json();
         })
         .then(function(json){
             var attributes = processData(json);
-            minValue = calculateMinValue(json);
+            minValue = calcMinValue(json);
             //call function to create proportional symbols
             createPropSymbols(json, attributes);
             createSequenceControls(attributes);
